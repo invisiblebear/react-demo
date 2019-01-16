e16638a9b0dba7113b6f14e450cfec5ddfbc4c44
jmitchell <jacob.mitchell@vml.com>
2019-01-16 13:57:54 -0600

    shows how to add styles inline

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,16 +7,11 @@ class Counter extends Component {
         imageUrl: 'https://picsum.photos/200'
     };
 
-    styles = {
-        fontSize: 14,
-        fontWeight: 'bold'
-    }
-
     render() { 
         return( 
         <React.Fragment>
             {/* <img src={this.state.imageUrl} alt='200px image'/><br /> */}
-            <span style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <span style={{fontSize: 30}} className='badge badge-primary m-2'>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </React.Fragment>
         );
