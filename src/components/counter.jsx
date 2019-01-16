bf201e50e4dcb83b5d74ccf89ad19d9977e55657
jmitchell <jacob.mitchell@vml.com>
2019-01-16 17:08:16 -0600

    binding to this for event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,6 +8,11 @@ class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3','tag4']
     };
 
+    constructor() {
+        super(); // used to call the constructor in the parent class from a child class
+        this.handleIncrement = this.handleIncrement.bind(this); // used to bind to 'this' for event handlers
+    }
+
     renderTags() {
         if (this.state.tags.length === 0) {
             return <p>There are no tags!</p>
@@ -16,14 +21,32 @@ class Counter extends Component {
         }
     }
 
-    render() { 
+    handleIncrement() {
+        console.log('Increment Clicked');
+    }
+
+    render() {
         return( 
         <React.Fragment>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
             {this.state.tags.length === 0 && 'Please create a new tag!'}
             {this.renderTags()}
         </React.Fragment>
         );
     }
+
+    getBadgeClasses () {
+        let classes = 'badge m-1 badge-';
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        const x = 'Zero';
+        return count === 0 ? x : count;
+    };
 }
 
 export default Counter; // makes available for import into another component
