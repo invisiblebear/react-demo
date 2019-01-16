ca48dafa29d3eba0e67ea6d34aaf7a37d69da40e
jmitchell <jacob.mitchell@vml.com>
2019-01-16 16:31:26 -0600

    adds dynamic classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,10 +11,16 @@ class Counter extends Component {
         return( 
         <React.Fragment>
             {/* <img src={this.state.imageUrl} alt='200px image'/><br /> */}
-            <span style={{fontSize: 30}} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </React.Fragment>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
