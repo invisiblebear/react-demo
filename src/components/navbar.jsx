d5e71e82a91374f96507cea509454f3b998815ee
jmitchell <jacob.mitchell@vml.com>
2019-01-18 17:48:02 -0600

    uses object destructuring to cherry pick only the properties you need and remove the 'this.props' keyword.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 // Stateless Functional Component
 
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
     return ( 
         <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="javascript: void(0);">
                 NavBar
             </a>
             <span className="badge badge-pill badge-secondary">
-                Total Number of Products: {props.totalCounters}
+                Total Number of Products: {totalCounters}
             </span>
         </nav>
      );
