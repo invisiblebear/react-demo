5597a50fe98605eee1473f51590e1af08f4a87b7
jmitchell <jacob.mitchell@vml.com>
2019-01-18 17:43:22 -0600

    example of a stateless functional component props needs to be passed in and you do not use 'this' keyword

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( 
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="javascript: void(0);">
-                    NavBar
-                </a>
-                <span className="badge badge-pill badge-secondary">
-                    Total Number of Products: {this.props.totalCounters}
-                </span>
-            </nav>
-         );
-    }
+// Stateless Functional Component
+
+const NavBar = (props) => {
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="javascript: void(0);">
+                NavBar
+            </a>
+            <span className="badge badge-pill badge-secondary">
+                Total Number of Products: {props.totalCounters}
+            </span>
+        </nav>
+     );
 }
- 
+
 export default NavBar;
