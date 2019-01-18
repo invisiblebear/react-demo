17f3cf8ba435f96d698f8d6641819565f3e19450
jmitchell <jacob.mitchell@vml.com>
2019-01-18 16:42:48 -0600

    removes the local state from the counter component and makes the counter component a controlled component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,45 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-
-    state = { // contains elements to be used in the component and is PRIVITE to this componet
-        value: this.props.counter.value, // this.props includes all attributes we set in coutners componet and includes data we give to this component. Props are read only
-        id: this.props.counter.id
-    };
-
-    renderTags() {
-        if (this.state.tags.length === 0) {
-            return <p>There are no tags!</p>
-        } else {
-            return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-        }
-    }
-
-    handleIncrement = () => { //uses an arrow function to be able to bind event handlers the 'this' or the Counter object
-        this.setState({value: this.state.value + 1}) // updates the state to update the view. you have to tell react explicitly what has changed. it compares the DOM and virtual DOM and updates only the things that need to be updated.
-    }
-
     render() {
         return( 
         <div className="m-2">
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 
-            <button onClick={ this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+            <button onClick={ () => this.props.onIncrement(this.props.counter)} className='btn btn-secondary btn-sm'>Increment</button>
             {/* use an arrow function inline to pass an argument into a method for eventhandlers */}
 
-            <button onClick={() => this.props.onDelete(this.state.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+            <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
         </div>
         );
     }
 
     getBadgeClasses () {
         let classes = 'badge m-1 badge-';
-        classes += this.state.value === 0 ? 'warning' : 'primary';
+        classes += this.props.counter.value === 0 ? 'warning' : 'primary';
         return classes;
     }
 
     formatCount() {
-        const { value: count } = this.state;
+        const { value: count } = this.props.counter;
         const x = 'Zero';
         return count === 0 ? x : count;
     };
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -14,13 +14,37 @@ class Counters extends Component {
     handleDelete = (counterId) => {
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({counters: counters}); // can be written as this.setState({counters});
-    }
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({counters});
+    };
+
+    handleIncrement = counter => {
+        console.log('Increment');
+
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters});
+    };
 
     render() { 
         return ( 
             <div>
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter => (
-                    <Counter key={counter.id} counter={counter} onDelete={this.handleDelete}>
+                    <Counter 
+                        key={counter.id} 
+                        counter={counter} 
+                        onDelete={this.handleDelete} 
+                        onIncrement={this.handleIncrement}
+                    >
                         {/* value={counter.value} is like inputing to child componet in Angular */}
                     </Counter>
                 ))}
