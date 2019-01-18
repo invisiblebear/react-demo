d1f419f951fc4f7db4c2d65abb876d7c8ceee5b2
jmitchell <jacob.mitchell@vml.com>
2019-01-18 17:42:27 -0600

    added classes to style better

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,12 @@ class NavBar extends Component {
     render() { 
         return ( 
             <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="javascript: void(0);">Navbar</a>
+                <a className="navbar-brand" href="javascript: void(0);">
+                    NavBar
+                </a>
+                <span className="badge badge-pill badge-secondary">
+                    Total Number of Products: {this.props.totalCounters}
+                </span>
             </nav>
          );
     }
